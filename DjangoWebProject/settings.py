7858197cdf0bda8c8eedbbcad0a7d7a771fcf030
uwaunitmatching@gmail.com <cits32002015>
2015-09-11 14:10:21 +0800

    Revert "Updated database connection details"
    This reverts commit a703166a36fc426453f3e59c9867d484daaadf98.

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -1,4 +1,4 @@
-﻿"""
+"""
 Django settings for DjangoWebProject project.
 """
 
@@ -20,11 +20,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database',
-        'USER': 'b42b6658560e49',
-        'PASSWORD': '383daa65',
-        'HOST': 'ap-cdbr-azure-southeast-a.cloudapp.net',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': path.join(PROJECT_ROOT, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '',
     }
 }
