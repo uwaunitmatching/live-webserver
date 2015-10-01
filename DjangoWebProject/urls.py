f3f40cdf1fcea634f99f2ba0ede2513803d629ad
uwaunitmatching@gmail.com <cits32002015>
2015-10-01 16:30:35 +0800

    Redirect login to the admin panel
    Login button now takes you to the admin panel

--- DjangoWebProject/urls.py
+++ DjangoWebProject/urls.py
@@ -22,28 +22,30 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', my_homepage_view),
-    url(r'^admin', include(admin.site.urls)),
+    url(r'^admin$', include(admin.site.urls)),
+    url(r'^login$', Redirect('/admin'),
     url(r'^results$', results),
     url(r'^index$', my_homepage_view),
     url(r'^search$', search),
-    url(r'^login$',
-        'django.contrib.auth.views.login',
-        {
-            'template_name': 'app/login.html',
-            'authentication_form': BootstrapAuthenticationForm,
-            'extra_context':
-            {
-                'title':'Log in',
-                'year':datetime.now().year,
-            }
-        },
-        name='login'),
-    url(r'^logout$',
-        'django.contrib.auth.views.logout',
-        {
-            'next_page': '/',
-        },
-        name='logout'),
+
+    #url(r'^login$',
+    #    'django.contrib.auth.views.login',
+    #    {
+    #        'template_name': 'app/login.html',
+    #        'authentication_form': BootstrapAuthenticationForm,
+    #        'extra_context':
+    #        {
+    #            'title':'Log in',
+    #            'year':datetime.now().year,
+    #        }
+    #    },
+    #    name='login'),
+    #url(r'^logout$',
+    #    'django.contrib.auth.views.logout',
+    #    {
+    #        'next_page': '/',
+    #    },
+    #    name='logout'),
 
 
 
